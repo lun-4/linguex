b29008c245d8439c300d4c98073916cc11897deb
Luna <git@l4.pm>
2023-08-01 22:50:51 -0300

    set larger timeout for textgen-webui calls

--- lib/linguex/http.ex
+++ lib/linguex/http.ex
@@ -7,6 +7,7 @@ defmodule Linguex.LLM.HTTP do
   # ])
 
   plug(Tesla.Middleware.JSON, decode_content_types: ["application/json"])
+  plug(Tesla.Middleware.Timeout, timeout: 15_000)
 
   def generate!(url, data),
     do: post!(url <> "/api/v1/generate", data)
