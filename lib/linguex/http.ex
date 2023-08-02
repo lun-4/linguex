1283e05adf06df724e103b29872ec87c0e232204
Luna <git@l4.pm>
2023-08-01 23:30:28 -0300

    attempt to bodge timeout into working?

--- lib/linguex/http.ex
+++ lib/linguex/http.ex
@@ -6,8 +6,8 @@ defmodule Linguex.LLM.HTTP do
   # plug(Tesla.Middleware.Headers, [
   # ])
 
-  plug(Tesla.Middleware.JSON, decode_content_types: ["application/json"])
   plug(Tesla.Middleware.Timeout, timeout: 15_000)
+  plug(Tesla.Middleware.JSON, decode_content_types: ["application/json"])
 
   def generate!(url, data),
     do: post!(url <> "/api/v1/generate", data)
