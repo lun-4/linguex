8f6865a8fad5d6ce22c219f09e8fcddf3d0d5daa
Luna <git@l4.pm>
2024-01-05 17:37:16 -0300

    make database config required for prod

--- config/runtime.exs
+++ config/runtime.exs
@@ -1,6 +1,24 @@
 import Config
 
+required_env! = fn env_name ->
+  System.get_env(env_name) ||
+    raise """
+    missing #{env_name} env
+    """
+end
 
 if config_env() == :prod do
-  config :linguex, Linguex.Repo, database: "./linguex.db"
+  database_host = required_env!.("DATABASE_HOSTNAME")
+  database_port = required_env!.("DATABASE_PORT")
+  database_name = required_env!.("DATABASE_NAME")
+  database_username = required_env!.("DATABASE_USERNAME")
+  database_password = required_env!.("DATABASE_PASSWORD")
+
+  config :linguex, Linguex.Repo,
+    database: database_name,
+    username: database_username,
+    password: database_password,
+    hostname: database_host,
+    port: database_port,
+    types: Linguex.PostgrexTypes
 end
